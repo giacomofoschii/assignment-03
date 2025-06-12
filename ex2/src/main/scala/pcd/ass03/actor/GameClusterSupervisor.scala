da36f53615ee50ca0d10f63e62aa7d7cd86b83e6
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2025-06-12 13:48:43 +0200

    [EX2] Still errors but less

--- ex2/src/main/scala/pcd/ass03/actor/GameClusterSupervisor.scala
+++ ex2/src/main/scala/pcd/ass03/actor/GameClusterSupervisor.scala
@@ -2,17 +2,31 @@ package pcd.ass03.actor
 
 import akka.actor.typed.*
 import akka.actor.typed.scaladsl.*
+import akka.cluster.ClusterEvent
+import akka.cluster.ClusterEvent.*
+import akka.cluster.ddata.typed.scaladsl.DistributedData
 import akka.cluster.typed.*
 import com.typesafe.config.Config
-
 import pcd.ass03.actor.{FoodManager, PlayerRegistry, WorldManager}
 
 object GameClusterSupervisor:
   def apply(): Behavior[Nothing] =
     Behaviors.setup[Nothing]: context =>
       val cluster = Cluster(context.system)
-      context.log.info(s"Starting node with roles: ${cluster.selfMember.roles}")
       
+      val worldManager = context.spawn(
+        Behaviors.supervise(WorldManager(context.system.settings.config))
+          .onFailure[Exception](SupervisorStrategy.restart), "WorldManagerSupervisor")
+      
+      cluster.subscriptions ! Subscribe(
+        context.messageAdapter[MemberEvent](ClusterEvent),
+        classOf[MemberEvent]
+      )
+      
+      Behaviors.receiveMessage:
+        case ClusterEvent(MemberRemoved(member, _)) =>
+          Behaviors.same
+          
       val worldManagerProxy = ClusterSingleton(context.system).init(
         SingletonActor(WorldManager(context.system.settings.config), "WorldManager"))
         
